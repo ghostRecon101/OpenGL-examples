111003a392e2a4929ce3eaa7a667f280f6419437
Vishal Sampath <visjun7@gmail.com>
2017-11-15 17:46:24 +0530

    TO DO: Error handling

--- OpenGLSample/OpenGLSample/res/shaders/Basic.shader
+++ OpenGLSample/OpenGLSample/res/shaders/Basic.shader
@@ -15,6 +15,6 @@ layout(location = 0) out vec4 color;
 
 void main()
 {
-	color = vec4(0.4, 0.1, 0.8, 1.0);
+	color = vec4(0.0, 0.0, 0.8, 1.0);
 };
 
--- OpenGLSample/OpenGLSample/src/Application.cpp
+++ OpenGLSample/OpenGLSample/src/Application.cpp
@@ -117,12 +117,25 @@ int main(void)
 	}
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
+	
+	float positions[] = { 
+		-0.5f, -0.5f, 
+		0.5f, -0.5f, 
+		0.5f, 0.5f,
+		-0.5f, 0.5f,
+	};
+
+	unsigned int indices[] = {0, 1, 2, 2, 3, 0};
 
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
-	float positions[6] = { -0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f };
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
@@ -143,7 +156,17 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		for each (float &var in positions)
+		{
+			var += 0.001f;
+		}
+
+		glBindBuffer(GL_ARRAY_BUFFER, buffer);
+		glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+		
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		glfwSwapBuffers(window);
 
